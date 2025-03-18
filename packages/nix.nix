75948c8892bbd36e7ede8b8cda2aa01e0ba0f67c
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-18 14:21:52 +0000

    feat(nix): add patched version of nix that spits information about fod to stderr

---
+++ packages/nix.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+pkgs.nixVersions.latest.overrideAttrs (_: {
+  prePatch = ''
+    substituteInPlace src/nix-env/nix-env.cc \
+      --replace 'settings.readOnlyMode = true' 'settings.readOnlyMode = false'
+  '';
+  patches = [ ./nix.patch ];
+})
