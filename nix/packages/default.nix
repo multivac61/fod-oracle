73c21e851583067f753a64460d74afa61ad7427c
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-19 11:30:04 +0000

    feat(nix): make default package build again

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -1,15 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, flake, ... }:
 with pkgs;
 buildGoModule {
   pname = "fod-oracle";
   version = "0.0";
-  src = ./.;
+  src = flake;
   vendorHash = "sha256-Z+VxAWb9jnF3L+7e+3zBQJu3pOHaBw3CyPoOTcKiLHI=";
 
-  # Add nix-eval-jobs as a runtime dependency
   nativeBuildInputs = [ makeWrapper ];
-
-  # Wrap the binary to ensure nix-eval-jobs is in PATH
   postFixup = ''
     wrapProgram $out/bin/fod-oracle \
       --prefix PATH : ${lib.makeBinPath [ pkgs.nix-eval-jobs ]}
