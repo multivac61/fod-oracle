d44e6d95ec49a5677216402fa833327bf9f1c958
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-21 12:10:04 +0000

    feat: simplify cloudflare-caddy package...

--- nix/packages/cloudflare-caddy.nix
+++ nix/packages/cloudflare-caddy.nix
@@ -1,30 +1,8 @@
 { pkgs, ... }:
-pkgs.stdenv.mkDerivation {
-  pname = "caddy-with-cloudflare";
-  version = pkgs.caddy.version;
-
-  meta.mainProgram = "caddy";
-
-  dontUnpack = true;
-
-  nativeBuildInputs = with pkgs; [
-    xcaddy
-    go
-    cacert
-    git
+pkgs.caddy.withPlugins {
+  hash = "sha256-wSgBOXa+OnZkwIKebSmvOizxZee5Zg/g3Di7fEO4FsI=";
+  plugins = [
+    "github.com/WeidiDeng/caddy-cloudflare-ip@v0.0.0-20231130002422-f53b62aa13cb"
+    "github.com/caddy-dns/cloudflare@v0.0.0-20250228175314-1fb64108d4de"
   ];
-
-  buildPhase = ''
-    # Set up Go environment
-    export GOCACHE=$TMPDIR/go-cache
-    export GOPATH="$TMPDIR/go"
-    export HOME=$TMPDIR
-
-    ${pkgs.xcaddy}/bin/xcaddy build --with github.com/caddy-dns/cloudflare
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp caddy $out/bin/
-  '';
 }
