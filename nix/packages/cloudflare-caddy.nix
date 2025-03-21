80d6d527619bb4ca4f3b3d42419b583332db7e7a
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-21 10:49:21 +0000

    feat: get cloudflare-caddy to build

---
+++ nix/packages/cloudflare-caddy.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+pkgs.stdenv.mkDerivation {
+  pname = "caddy-with-cloudflare";
+  version = pkgs.caddy.version;
+
+  dontUnpack = true;
+
+  nativeBuildInputs = with pkgs; [
+    xcaddy
+    go
+    cacert
+    git
+  ];
+
+  buildPhase = ''
+    # Set up Go environment
+    export GOCACHE=$TMPDIR/go-cache
+    export GOPATH="$TMPDIR/go"
+    export HOME=$TMPDIR
+
+    ${pkgs.xcaddy}/bin/xcaddy build --with github.com/caddy-dns/cloudflare
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp caddy $out/bin/
+  '';
+}
+
